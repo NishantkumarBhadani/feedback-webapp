b0815e106a6857fac3575e68e9946113b411e928
NishantkumarBhadani <kumarbhadanin@gmail.com>
2026-03-13 16:43:38 +0530

    complete user signup api

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -7,18 +7,87 @@ export async function POST(request: Request) {
     await dbConnect()
 
     try {
-        const {username, email, password} = await request.json()
+        const { username, email, password } = await request.json()
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified: true
+        })
+        if (existingUserVerifiedByUsername) {
+            return Response.json({
+                success: false,
+                message: "Username is already taken."
+            }, {
+                status: 400
+            })
+        }
+
+        const existingUserByEmail=await UserModel.findOne({
+            email
+        })
+        const verifyCode=Math.floor(100000 + Math.random()*900000).toString();
+        if(existingUserByEmail){
+            if(existingUserByEmail.isVerified){
+                return Response.json({
+                success:false,
+                message:"User already exist with this email"
+            },{status:400})
+            }
+            else{
+                const hashedPassword=await bcrypt.hash(password,10);
+                existingUserByEmail.password=hashedPassword;
+                existingUserByEmail.verifyCode=verifyCode;
+                existingUserByEmail.verifyCodeExpiry=new Date(Date.now()+3600000)
+                await existingUserByEmail.save();
+            }
+        } else{
+            const hashedPassword =await bcrypt.hash(password,10)
+            const expiryDate=new Date()
+            expiryDate.setHours(expiryDate.getHours()+1)
+
+            const newUsesr=new UserModel({
+                username,
+                email,
+                password:hashedPassword,
+                verifyCode,
+                verifyCodeExpiry:expiryDate,
+                isVerified:false,
+                isAcceptingMessages:true,
+                message:[]
+            })
+
+            await newUsesr.save()
+        }
+
+        //Send verification Email
+        const emailResponse=await sendVerificationEmails(
+            email,
+            username,
+            verifyCode
+        )
+
+        if(!emailResponse.success){
+            return Response.json({
+                success:false,
+                message:emailResponse.message
+            },{status:500})
+        }
+
+        return Response.json({
+            success:true,
+            message:"User registered successfully. Please verify your email"
+        },{status:201})
+
     } catch (error) {
         console.error("Error using resgistering error", error);
         return Response.json(
-        {
-            success: false,
-            message: "Error refistering user"
-        },
-        {
-            status: 500
-        }
-    )
+            {
+                success: false,
+                message: "Error refistering user"
+            },
+            {
+                status: 500
+            }
+        )
 
     }
 }
